9bdacf82d6a69cde48bf53ed5436f465b589b7e3
Chandrakumar K <chandrakumar.k@bloomenergy.com>
2025-03-21 10:24:45 +0530

    IsOdd function added

--- sample.py
+++ sample.py
@@ -1,5 +1,9 @@
 def sample():
     return "This is a sample python file."
 
+def is_odd(num):
+    return num % 2 != 0
+
 print("Welcome Guys!!")
 print(sample())
+print(is_odd(input('Enter a number: ')))
